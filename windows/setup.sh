1cf40d4cdfc0fc868dcf8d99791e3884e2954e17
Ed Yu <edyu@users.noreply.github.com>
2023-06-25 12:14:50 -0700

    Update to use snap

--- windows/setup.sh
+++ windows/setup.sh
@@ -1,8 +1,10 @@
-#windows
-# install systemd by creating/editing the file `/etc/wsl.conf`
-[boot]
-systemd=true
+## windows
+# install systemd by overwrite/copy the file `/etc/wsl.conf`
+# snap requires systemd
+cp wsl.conf /etc/wsl.conf
 # restart wsl with `wsl --shutdown`
+
+# sync time
 sudo apt install chrony
 sudo systemctl restart chrony.service
 
@@ -11,16 +13,16 @@ sudo systemctl restart chrony.service
 New-Item -Path $PROFILE -ItemType SymbolicLink -Value C:\Users\edlyu\ws\dotfiles\windows\pwsh\Microsoft.PowerShell_profile.ps1
 #New-Item -Path C:\Users\edlyu\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value C:\Users\edlyu\ws\dotfiles\windows\pwsh\Microsoft.PowerShell_profile.ps1
 # ln C:\Users\edlyu\ws\dotfiles\windows\pwsh\Microsoft.PowerShell_profile.ps1 C:\Users\edlyu\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
-choco install colortools
-cd c:\ProgramData\chocolatey\lib\colortool\content
-download Nord.itermcolors
+#choco install colortools
+#cd c:\ProgramData\chocolatey\lib\colortool\content
+#download Nord.itermcolors
 
 #nvim
 mkdir C:\Users\edlyu\.config
-ln C:\Users\edlyu\ws\dotfiles\windows\nvim C:\Users\edlyu\.config\nvim
-ln C:\Users\edlyu\ws\dotfiles\windows\nvim C:\Users\edlyu\AppData\Local\nvim
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+#ln C:\Users\edlyu\ws\dotfiles\windows\nvim C:\Users\edlyu\.config\nvim
+#ln C:\Users\edlyu\ws\dotfiles\windows\nvim C:\Users\edlyu\AppData\Local\nvim
+#iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+#    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 
 
 #terminal
@@ -29,29 +31,26 @@ cp C:\Users\edlyu\ws\dotfiles\windows\terminal\settings.json C:\Users\edlyu\AppD
 #alacritty
 ln C:\Users\edlyu\ws\dotfiles\windows\alacritty\alacritty.yml C:\Users\edlyu\AppData\Roaming\alacritty\alacritty.yaml 
 
-#wsl
-#  fish
+## wsl
+# fish
+# need 3.4+ but ubuntu 22.04 only installs 3.3.1
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
-sudo apt install fish
+sudo apt upgrade
 ln -s ~/ws/dotfiles/windows/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/ws/dotfiles/windows/fish/fishfile ~/.config/fish/fishfile
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+ln -s ~/ws/dotfiles/windows/fish/fish_plugins ~/.config/fish/fish_plugins
+fisher update
 #fisher install ilancosman/tide
-fisher install jorgebucaran/hydro
+#fisher install jorgebucaran/hydro
 #ln -s ~/ws/dotfiles/windows/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
-#  nvim
-# install 0.7+ using ppa
-# https://sharksforarms.dev/posts/neovim-rust/
-# sudo add-apt-repository ppa:neovim-ppa/stable 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-# in nvim :PlugInstall
-# lsp needs unzip
-# sudo apt install unzip
-ln -s ~/ws/dotfiles/windows/nvim ~/.config/nvim
-
-# setup wsl.conf for microsoft-edge
-# vi /etc/wsl.conf
-# [boot]
-# systemd=true
+# git
+ln -s git/gitconfig ~/.gitconfig
+# nvim
+sudo snap install nvim --classic
+git clone git@github.com:AstroNvim/AstroNvim ~/.config/nvim
+git clone git@github.com:edyu/astronvim ~/.config/nvim/lua/user
+# node
+sudo snap install node --classic
+# zig
+sudo snap install zig --edge --classic
